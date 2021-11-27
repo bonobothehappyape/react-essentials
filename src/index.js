94f3870098144f36316057a584fccd25ba5fce60
Dimitris Asvestis <dimitris.asvestis@gmail.com>
2021-11-27 20:04:38 +0100

    - properties

--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(
-  <ul>
-    <li>Monday</li>
-    <li>Tuesday</li>
-    <li>Wednesday</li>
-  </ul>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
