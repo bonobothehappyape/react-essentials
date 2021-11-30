20ca1e2a04582d66914841ebdee66b33657f50bd
Dimitris Asvestis <dimitris.asvestis@gmail.com>
2021-11-30 09:55:17 +0100

    - keys to properties list

--- src/App.js
+++ src/App.js
@@ -15,6 +15,11 @@ function Main(props) {
       <p>
         We serve the most {props.adjective} food around.
       </p>
+      <ul style={{ textAlign: "left" }}>
+        {props.dishes.map(dish => (
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
     </section>
   );
 }
@@ -27,11 +32,22 @@ function Footer(props) {
   );
 }
 
+const dishes = [
+  "Macaroni and Cheese",
+  "Salmon",
+  "Tofu with Vegetables"
+];
+
+const dishObjects = dishes.map((dish, i) => ({
+  id: i,
+  title: dish
+}));
+
 function App() {
   return (
     <div className="App">
       <Header name="Horacio" />
-      <Main adjective="amazing" />
+      <Main adjective="amazing" dishes={dishObjects} />
       <Footer year={new Date().getFullYear()} />
     </div>
   );
