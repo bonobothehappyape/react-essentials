4950c5330c9860711e309143a7cfbf58ed9c72c8
Dimitris Asvestis <dimitris.asvestis@gmail.com>
2021-12-05 16:38:01 +0100

    - conditional rendering

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App authorized={true} />,
+  document.getElementById("root")
+);
