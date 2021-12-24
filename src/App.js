33f7ae6fb7a6ae08cc9e5d51f85e147afb875d5a
Dimitris Asvestis <dimitris.asvestis@gmail.com>
2021-12-24 18:07:00 +0100

    - loading states

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 // https://api.github.com/users/bonobothehappyape
 
-function App() {
+function App(login) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
