bd7cde34e15eeffa18f372c3aaa6ebda3701d830
Dimitris Asvestis <dimitris.asvestis@gmail.com>
2021-12-26 16:20:55 +0100

    - testing hooks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 
-// https://api.github.com/users/bonobothehappyape
-
-function App({login}) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then(response => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, []);
-
-  if (loading) return <h1>Loading...</h1>;
-
-  if (error) {
-    return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  }
-
-  if (data) {
-    return (
-      <div>
-        <h1>name: {data.name}</h1>
-        <p>location: {data.location}</p>
-        <img alt={data.login} src={data.avatar_url} />
-      </div>
-    );
-  }
-
-  return <div>No Data Available</div>;
+function App() {
+  return <h1>Hello React Testing Library</h1>;
 }
 
 export default App;
